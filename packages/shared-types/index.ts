27d765a6f61258b012a8048e91091ffd6dabb4a9
kevs-mist <kevalmistry5927@gmail.com>
2026-07-26 14:01:28 +0530

    Initial commit: monorepo scaffolding with Turborepo, Next.js apps, and shared packages

---
+++ packages/shared-types/index.ts
@@ -0,0 +1,2 @@
+// Placeholder - types will be defined in a later task
+export const TEST_EXPORT = 'shared-types-test';
---
+++ packages/supabase-client/index.ts
@@ -0,0 +1,2 @@
+// Placeholder - Supabase client factory will be implemented in a later task
+export const TEST_EXPORT = 'supabase-client-test';
